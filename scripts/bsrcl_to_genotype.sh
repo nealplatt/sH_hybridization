c34aa266e264a6cf3891d63f0d70d9b783337332
Neal Platt <neal.platt@gmail.com>
2018-03-09 09:56:34 -0600

    updated structure of nested loops for job submission, added job limiting while loop

--- scripts/bsrcl_to_genotype.sh
+++ scripts/bsrcl_to_genotype.sh
@@ -14,13 +14,17 @@ source master/nplatt/sH_hybridizationscripts/set_env.sh
 
 cd $BSRCL_DIR
 
+
+MAX_JOBS_ALLOWED=300
+NUM_JOBS_IN_QUEUE=$(qstat | grep nplatt | wc -l)
+
 for SAMPLE in $(cat $SAMPLE_LIST); do
 
     # MERGE_INDIV_GVCF ---------------------------------------------------------
     ls $SAMPLE"_interval_"*".g.vcf" >$SAMPLE.list      
 
     MERGE_JOB_NAME=$SAMPLE".merge_indiv"
-    THREADS=1
+    THREADS=12
 
     IN_LIST=$SAMPLE.list
     OUT_GVCF="tmp_$SAMPLE.merged.g.vcf"
@@ -32,7 +36,7 @@ for SAMPLE in $(cat $SAMPLE_LIST); do
 
     cat scripts/$MERGE_JOB_NAME.sh | $MERGE_QSUB
 
-    
+
     # SORT_INDIV_GVCF ----------------------------------------------------------
     SORT_JOB_NAME=$SAMPLE".sort_indiv"
     THREADS=12
@@ -47,28 +51,40 @@ for SAMPLE in $(cat $SAMPLE_LIST); do
 
     cat scripts/$SORT_JOB_NAME.sh | $SORT_QSUB
 
-    # GDBIMPORT ----------------------------------------------------------------
-    for each INTERVAL in $(cat INVERVAL.LIST); do
+done
 
-        GDBIMPORT_JOB_NAME=$SAMPLE".$INTERVAL"
-        THREADS=12
+#CHECK FOR COMPLETION
 
-        IN_GVCF="$SAMPLE.g.vcf"
-        OUT_DB="db/$INTERVAL"
+
+
+# GDBIMPORT ----------------------------------------------------------------
+for INTERVAL in $(cat $INTERVALS_DIR/all_filtered_intervals.list); do
+    GDBIMPORT_JOB_NAME=$SAMPLE".$INTERVAL"
+    THREADS=12
+
+    IN_GVCF="$SAMPLE.g.vcf"
+    OUT_DB="db/$INTERVAL"
     
-        GDBIMPORT="$SINGULARITY gatk --java-options \"-Xmx4g -Xms4g\" GenomicsDBImport \
-            -V $SAMPLE_LIST \
-            --genomicsdb-workspace-path $OUT_DB \
-            -L $INTERVAL
-            --reader-threads $THREADS
-            --batch-size 24"
+    GDBIMPORT="$SINGULARITY gatk --java-options "'"-Xmx4g -Xms4g"'" GenomicsDBImport \
+        -V $SAMPLE_LIST \
+        --genomicsdb-workspace-path $OUT_DB \
+        -L $INTERVAL \
+        --reader-threads $THREADS \
+        --batch-size 24"
 
-        $GDBIMPORT_QSUB="$QSUB -pe mpi $THREADS -N $GDBIMPORT_JOB_NAME -o logs/$GDBIMPORT_JOB_NAME.log -hold_jid $SORT_JOB_NAME"
-        echo $GDBIMPORT >scripts/$GDBIMPORT_JOB_NAME.sh
+    GDBIMPORT_QSUB="$QSUB -pe mpi $THREADS -N $GDBIMPORT_JOB_NAME -o logs/$GDBIMPORT_JOB_NAME.log -hold_jid $SORT_JOB_NAME"
+    echo $GDBIMPORT >scripts/$GDBIMPORT_JOB_NAME.sh
 
-        cat scripts/$GDBIMPORT_JOB_NAME.sh | $GDBIMPORT
+    #only submit a limited number of jobs at a time...(dont overload queue)
+    NUM_JOBS_IN_QUEUE=$(qstat | grep nplatt | wc -l)
+
+    while [ $NUM_JOBS_IN_QUEUE -gt $MAX_JOBS_ALLOWED ]; do
+        sleep 1s
+        echo -n "."
+        NUM_JOBS_IN_QUEUE=$(qstat | grep nplatt | wc -l)
     done
-
+        
+    cat scripts/$GDBIMPORT_JOB_NAME.sh | $GDBIMPORT
 done
 
 #CHECK FOR COMPLETION
