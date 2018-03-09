354a17cf810ab255e3aa5ac52dffc160eba22ee5
Neal Platt <neal.platt@gmail.com>
2018-03-09 16:58:42 -0600

    add bsrcl_hc.sh code, need to modify for round2

--- scripts/bsrcl_hc_r2.sh
+++ scripts/bsrcl_hc_r2.sh
@@ -13,6 +13,66 @@ source master/nplatt/sH_hybridizationscripts/set_env.sh
 
 cd $BSRCL_DIR
 
+for SAMPLE in $(cat $SAMPLE_LIST); do
+    for INTERVAL in $(seq -w 0 49); do    
+    
+        # HC -------------------------------------------------------------------
+        HC_JOB_NAME=$SAMPLE".r2."$INTERVAL.hc
+        THREADS=1
+
+        IN_BED="$INTERVALS_DIR/filtered_interval.part$INTERVAL.list"
+        IN_BAM=$MAP_DIR/$SAMPLE"_processed.bam"
+        IN_BSQR_TABLE="88888888888888888888888888888888888888888888888888"
+        OUT_GVCF=$BSRCL_DIR"/hc_vcf_r2/"$SAMPLE"_interval_"$INTERVAL".g.vcf"
+    
+        HC="$SINGULARITY gatk HaplotypeCaller \
+            -I $IN_BAM \
+            -O $OUT_GVCF \
+            -R $REFERENCE \
+            -L $IN_BED \
+            -BQSR post_recal_data.1.table \
+            -ERC GVCF"
+
+        HC_QSUB="$QSUB -pe mpi $THREADS -N $HC_JOB_NAME -o logs/$HC_JOB_NAME.log"
+        echo $HC >scripts/$HC_JOB_NAME.sh
+    
+        cat scripts/$HC_JOB_NAME.sh | $HC_QSUB
+
+    done
+done
+
+
+# HC-CHECKER------------------------------------------------------------
+PASSED=0
+FAILED=0
+TOTAL=0
+NUM_SAMPLES=0
+EXPECTED=$(expr 96 \* 50)
+
+for SAMPLE in $(cat $SAMPLE_LIST); do
+    
+    for INTERVAL in $(seq -w 0 49); do
+        HC_JOB_NAME=$SAMPLE"."$INTERVAL.hc
+        LOG="logs/$HC_JOB_NAME.log"
+
+        if [[ $(grep "HaplotypeCaller done" $LOG) ]]; then
+            PASSED=$((PASSED+1))
+        else
+            FAILED=$((FAILED+1))
+            HC_JOB_NAME=$SAMPLE"."$INTERVAL.hc
+            THREADS=4
+            HC_QSUB="$QSUB -pe mpi $THREADS -N $HC_JOB_NAME -o logs/$HC_JOB_NAME.log"
+            cat scripts/$HC_JOB_NAME.sh | $HC_QSUB
+        fi 
+
+    NUM_SAMPLES=$((NUM_SAMPLES+1))
+    TOTAL=$((TOTAL+1))
+    done
+done 
+
+echo -e "PASSED\tFAILED\tTOTAL\tNUM_SAMPLES\tEXPECTED"
+echo -e "$PASSED\t$FAILED\t$TOTAL\t$NUM_SAMPLES\t$EXPECTED"
+
 #-------------------------------------------------------------------------------
 #NOTES FROM GATK FORUM:
 #https://gatkforums.broadinstitute.org/gatk/discussion/4913/recommended-protocol-for-bootstrapping-haplotypecaller-and-baserecalibrator-outputs
