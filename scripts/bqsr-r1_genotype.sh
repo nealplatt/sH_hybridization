29cc75c79702ff6c4c6711fae713326933e089e0
Neal Platt <neal.platt@gmail.com>
2018-03-11 10:30:50 -0500

    random updates

--- scripts/bqsr-r1_genotype.sh
+++ scripts/bqsr-r1_genotype.sh
@@ -66,8 +66,8 @@ for INTERVAL in $(ls db); do
         FAILED=$((FAILED+1))
         THREADS=4
     
-    GENOTYPE_QSUB="$QSUB -pe mpi $THREADS -N $GENOTYPE_JOB_NAME -o logs/$GENOTYPE_JOB_NAME.log"
-    cat scripts/$GENOTYPE_JOB_NAME.sh | $GENOTYPE_QSUB
+        GENOTYPE_QSUB="$QSUB -pe mpi $THREADS -N $GENOTYPE_JOB_NAME -o logs/$GENOTYPE_JOB_NAME.log"
+        cat scripts/$GENOTYPE_JOB_NAME.sh | $GENOTYPE_QSUB
     fi
 
     NUM_SAMPLES=$((NUM_SAMPLES+1))
