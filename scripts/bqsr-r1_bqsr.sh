c05acc03c7b468a455ff74296e4f21674af4a04a
Neal Platt <neal.platt@gmail.com>
2018-03-11 13:56:11 -0500

    various changes

--- scripts/bqsr-r1_bqsr.sh
+++ scripts/bqsr-r1_bqsr.sh
@@ -17,8 +17,8 @@ cd $BSRCL_DIR
 SELECT_SNPS_JOB_NAME=cohort_select_snps
 THREADS=1
 
-IN_VCF="$BSRCL_DIR/cohort_raw.g.vcf"
-OUT_VCF="$BSRCL_DIR/cohort_raw_snps.g.vcf"
+IN_VCF="$BSRCL_DIR/cohort_preBSQR.g.vcf"
+OUT_VCF="$BSRCL_DIR/cohort_preBSQR_rawSNPS.g.vcf"
     
 SELECT_SNPS="$SINGULARITY gatk SelectVariants \
     -V $IN_VCF \
@@ -36,18 +36,15 @@ cat scripts/$SELECT_SNPS_JOB_NAME.sh | $SELECT_SNPS_QSUB
 FILTER_SNPS_JOB_NAME="cohort_filter_snps"
 THREADS=1
 
-IN_VCF=$OUT_VCF
+IN_VCF="$BSRCL_DIR/cohort_preBSQR_rawSNPS.g.vcf"
 OUT_VCF="$BSRCL_DIR/cohort_filtered_snps.g.vcf"
 
-FILTER_EXPRESSION="QD < 2.0 || FS > 60.0 || MQ < 40.0 || MQRankSum < -12.5 || ReadPosRankSum < -8.0"
-FILTER_NAME="recommended_snp_filter"
-    
 FILTER_SNPS="$SINGULARITY gatk VariantFiltration \
     -R $REFERENCE \
     -V $IN_VCF \
-    --filterExpression $FILTER_EXPRESSION \
-    --filterName $FILTER_NAME \
-    -o $OUT_VCF"
+    --filter-expression "'"QD < 2.0 || FS > 60.0 || MQ < 40.0 || MQRankSum < -12.5 || ReadPosRankSum < -8.0"'" \
+    --filter-name "'"recommended_snp_filter"'" \
+    -O $OUT_VCF"
 
 FILTER_SNPS_QSUB="$QSUB -pe mpi $THREADS -N $FILTER_SNPS_JOB_NAME -o logs/$FILTER_SNPS_JOB_NAME.log -hold_jid $SELECT_SNPS_JOB_NAME"
 
@@ -59,8 +56,8 @@ cat scripts/$FILTER_SNPS_JOB_NAME.sh | $FILTER_SNPS_QSUB
 SELECT_INDELS_JOB_NAME="cohort_select_indels"
 THREADS=1
 
-IN_VCF="$BSRCL_DIR/cohort_raw.g.vcf"
-OUT_VCF="$BSRCL_DIR/cohort_raw_indels.g.vcf"
+IN_VCF="$BSRCL_DIR/cohort_preBSQR.g.vcf"
+OUT_VCF="$BSRCL_DIR/cohort_preBSQR_rawIndels.g.vcf"
     
 SELECT_INDELS="$SINGULARITY gatk SelectVariants \
     -V $IN_VCF \
@@ -79,18 +76,15 @@ cat scripts/$SELECT_JOB_NAME.sh | $SELECT_INDELS_QSUB
 FILTER_INDELS_JOB_NAME="cohort_filter_indels"
 THREADS=1
 
-IN_VCF=$OUT_VCF
+IN_VCF="$BSRCL_DIR/cohort_preBSQR_rawIndels.g.vcf"
 OUT_VCF="$BSRCL_DIR/cohort_filtered_indels.g.vcf"
 
-FILTER_EXPRESSION="QD < 2.0 || FS > 200.0 || ReadPosRankSum < -20.0"
-FILTER_NAME="recommended_indel_filter"
-        
 FILTER_INDELS="$SINGULARITY gatk VariantFiltration \
     -R $REFERENCE \
     -V $IN_VCF \
-    --filterExpression $FILTER_EXPRESSION \
-    --filterName $FILTER_NAME \
-    -o $OUT_VCF"
+    --filter-expression "'"QD < 2.0 || FS > 200.0 || ReadPosRankSum < -20.0"'" \
+    --filter-name "'"recommended_indel_filter"'" \
+    -O $OUT_VCF"
 
 FILTER_INDELS_QSUB="$QSUB -pe mpi $THREADS -N $FILTER_INDELS_JOB_NAME -o logs/$FILTER_INDELS_JOB_NAME.log -hold_jid $SELECT_INDELS_JOB_NAME"
 
@@ -111,7 +105,7 @@ MERGE="$SINGULARITY gatk CombineVariants \
    -R $REFERENCE \
    --variant:snps $IN_SNP_VCF \
    --variant:indels $IN_INDEL_VCF \
-   -o $OUT_VCF \
+   -O $OUT_VCF \
    -genotypeMergeOptions PRIORITIZE \
    -priority snps,indels"
 
@@ -128,31 +122,31 @@ cat scripts/$MERGE_JOB_NAME.sh | $MERGE_QSUB
 
 for BAM in $(ls $MAP_DIR/*_processed.bam); do
 
-    $SAMPLE=$(basename $BAM _processed.bam)
+    SAMPLE=$(basename $BAM _processed.bam)
 
     # RECAL SCORE/OBSERVE READS-------------------------------------------------    
 
     RECAL_OBSERVE_JOB_NAME="$SAMPLE.recal_score_reads"
-    THREADS=1
+    THREADS=12
 
     IN_BAM=$MAP_DIR/$BAM
-    IN_VCF="$BSRCL_DIR/cohort_filtered_snps-indels.g.vcf"
+    IN_VCF="$BSRCL_DIR/cohort_filtered_snps.g.vcf"
     OUT_TABLE=$SAMPLE"_recal-1_data.table"
     
     RECAL_OBSERVE="$SINGULARITY gatk BaseRecalibrator \
         -R $REFERENCE \
-        -I $BAM_FILE \
-        -knownSites $IN_VCF \
-        -o $OUT_TABLE"
+        -I $BAM \
+        --known-sites $IN_VCF \
+        -O $OUT_TABLE"
 
     RECAL_OBSERVE_QSUB="$QSUB -pe mpi $THREADS -N $RECAL_OBSERVE_JOB_NAME -o logs/$RECAL_OBSERVE_JOB_NAME.log -hold_jid $FILTER_SNPS_JOB_NAME"
 
     echo $RECAL_OBSERVE >scripts/$RECAL_OBSERVE_JOB_NAME.sh
     cat scripts/$RECAL_OBSERVE_JOB_NAME.sh | $RECAL_OBSERVE_QSUB
 
-    # RECAL MODIFY READS -----------------------------------------------------------    
-    RECAL_MODIFY_JOB_NAME="$SAMPLE.recal_modify_reads"
-    THREADS=1
+    # RECAL MODIFED READS -----------------------------------------------------------    
+    RECAL_MODIFY_JOB_NAME="$SAMPLE.recal_modifed_reads"
+    THREADS=12
 
     IN_BAM=$IN_BAM
     IN_VCF=$IN_VCF
@@ -161,26 +155,26 @@ for BAM in $(ls $MAP_DIR/*_processed.bam); do
     
     RECAL_MODIFY="$SINGULARITY gatk BaseRecalibrator \
         -R $REFERENCE \
-        -I $BAM_FILE \
-        -knownSites $IN_VCF \
-        -BQSR recal_data.1.table \
-        -o $OUT_TABLE"
+        -I $BAM \
+        --known-sites $IN_VCF \
+        --BQSR recal_data.1.table \
+        -O $OUT_TABLE"
 
-    RECAL_MODIFY_QSUB="$QSUB -pe mpi $THREADS -N $RECAL_OBSERVE_JOB_NAME -o logs/$RECAL_OBSERVE_JOB_NAME.log, -hold_jid $RECAL_OBSERVE_JOB_NAME"
+    RECAL_MODIFY_QSUB="$QSUB -pe mpi $THREADS -N $RECAL_MODIFY_JOB_NAME -o logs/$RECAL_MODIFY_JOB_NAME.log, -hold_jid $RECAL_OBSERVE_JOB_NAME"
 
     echo $RECAL_MODIFY >scripts/$RECAL_MODIFY_JOB_NAME.sh
     cat scripts/$RECAL_MODIFY_JOB_NAME.sh | $RECAL_MODIFY_QSUB    
 
     # RECAL ANALYZE COVARIATES -------------------------------------------------    
     ANALYZE_JOB_NAME="$SAMPLE.covariate_analysis"
-    THREADS=1
+    THREADS=12
 
     BEFORE_TABLE=$SAMPLE"_recal-1_data.table"
     AFTER_TABLE=$SAMPLE"_postrecal-1_data.table"
     OUT_PDF=$SAMPLE"_recalibration_plot.1.pdf"
 
     ANALYZE="$SINGULARITY gatk AnalyzeCovariates \
-        -R $REF \
+        -R $REFERENCE \
         -before $BEFORE_TABLE \
         -after $AFTER_TABLE \
         -plots $OUT_PDF"
