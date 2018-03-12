3f6da4c4910fa2a656b0912026f4e0c32b779c3f
nealplatt <neal.platt@gmail.com>
2018-03-12 13:41:31 -0500

    remove pausing sxn of script since genotyping is so fast

--- scripts/bqsr-r2_genotype.sh
+++ scripts/bqsr-r2_genotype.sh
@@ -4,75 +4,83 @@
 # NPlatt
 # Neal.platt@gmail.com
 
-# bsrcl_genotype - runs GATK's GenotypeGVCF on each sample/interval combination
+# bqsr-r2_genotype.sh - runs GATK's GenotypeGVCF on each sample/interval 
+#   combination
 
-# TODO(nplatt): add code to check that all combos run successfully
 # TODO(nplatt): update comments
 
-
+#import env variables
 source /master/nplatt/sH_hybridization/scripts/set-env.sh
 
+#move to working dir
 cd $BQSR_DIR
 
-NUM_JOBS_IN_QUEUE=0
-MAX_JOBS_ALLOWED=3000
-
+#all output will go to this dir
 mkdir genotype_interval_vcfs_r2
 
-for INTERVAL in $(ls db); do
+#genotype each interval in the db_r2 dir (qsub)
+for INTERVAL in $(ls db_r2); do
+    #job specific params    
     GENOTYPE_JOB_NAME=$INTERVAL".genotype_r2"
     THREADS=1
+    LOG="-o logs/$GENOTYPE_JOB_NAME.log"
+    DEPEND=""
 
+    #job specific in/out files
     IN_DB="gendb://db_r2/$INTERVAL"
     OUT_VCF="genotype_interval_vcfs_r2/$INTERVAL.vcf"
     
-    GENOTYPE="$SINGULARITY gatk GenotypeGVCFs -R $REFERENCE -V $IN_DB -new-qual -O $OUT_VCF"
-
-    GENOTYPE_QSUB="$QSUB -pe mpi $THREADS -N $GENOTYPE_JOB_NAME -o logs/$GENOTYPE_JOB_NAME.log"
+    #job specific script
+    GENOTYPE="$SINGULARITY gatk GenotypeGVCFs \
+        -R $REFERENCE \
+        -V $IN_DB \
+        -new-qual \
+        -O $OUT_VCF"
     echo $GENOTYPE >scripts/$GENOTYPE_JOB_NAME.sh
+    
+    #job specific qsub
+    JOB_QSUB="$QSUB -pe mpi $THREADS -N $GENOTYPE_JOB_NAME $LOG $DEPEND"
 
-
-    NUM_JOBS_IN_QUEUE=$(qstat | grep nplatt | wc -l)
-
-    while [ $NUM_JOBS_IN_QUEUE -gt $MAX_JOBS_ALLOWED ]; do
-        sleep 1s
-        echo -n "."
-        NUM_JOBS_IN_QUEUE=$(qstat | grep nplatt | wc -l)
-    done
-
-    cat scripts/$GENOTYPE_JOB_NAME.sh | $GENOTYPE_QSUB
+    #submit job to scheduler
+    cat scripts/$GENOTYPE_JOB_NAME.sh | $JOB_QSUB
     
 done
 
+################################################################################
+# Check log files to see that all ran to completion 
 
-#################################
-
-#CHECK FOR COMPLETION
-
+#reset variables
 PASSED=0
 FAILED=0
 TOTAL=0
-NUM_SAMPLES=0
-EXPECTED=$(expr 96 \* 50)
+EXPECTED=$(ls -d db_r2 | wc -l)
 
-for INTERVAL in $(ls db); do
+#check that each log file has "GenotypeGVCFs done" indicating run to completion
+for INTERVAL in $(ls db_r2); do
     GENOTYPE_JOB_NAME=$INTERVAL".genotype_r2"
-    LOG="logs/$GENOTYPE_JOB_NAME.log"
+    THREADS=1
+    LOG="-o logs/$GENOTYPE_JOB_NAME.log"
+    DEPEND=""
 
 
     if [[ $(grep "GenotypeGVCFs done" $LOG) ]]; then
         PASSED=$((PASSED+1))
     else
+        #resubmit with 12 threads (only reason doing this is to "hog" memory
+        #  from an entire node rather than sharing        
         FAILED=$((FAILED+1))
-        THREADS=4
+        THREADS=12
     
-        GENOTYPE_QSUB="$QSUB -pe mpi $THREADS -N $GENOTYPE_JOB_NAME -o logs/$GENOTYPE_JOB_NAME.log"
+        JOB_QSUB="$QSUB -pe mpi $THREADS -N $GENOTYPE_JOB_NAME $LOG $DEPEND"
         cat scripts/$GENOTYPE_JOB_NAME.sh | $GENOTYPE_QSUB
     fi
 
-    NUM_SAMPLES=$((NUM_SAMPLES+1))
     TOTAL=$((TOTAL+1))
 done
 
-echo -e "PASSED\tFAILED\tTOTAL\tNUM_SAMPLES\tEXPECTED"
-echo -e "$PASSED\t$FAILED\t$TOTAL\t$NUM_SAMPLES\t$EXPECTED"
+echo -e "PASSED\tFAILED\tTOTAL\tEXPECTED"
+echo -e "$PASSED\t$FAILED\t$TOTAL\t$EXPECTED"
+
+#           PASSED  FAILED  TOTAL   EXPECTED
+#1stPass
+
