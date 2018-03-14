b800c5f8144e2211d73f7363ecec608532467fdb
nealplatt <neal.platt@gmail.com>
2018-03-14 12:21:46 -0500

    created files form bqsr-r3

---
+++ scripts/bqsr_buildCohortVcf.sh
@@ -0,0 +1,173 @@
+#!/bin/bash
+#
+# SNP calling in S. haemotobium hybridzone(s).
+# NPlatt
+# Neal.platt@gmail.com
+
+# bqsr_buildCohortVcf.sh - after genotyping, build and filter cohort vcf for 
+#       recalibration
+
+# TODO(nplatt): update comments
+
+WAIT_FOR_CLEAR_QUEUE
+
+cd $ROUND"_cohort_VCF"
+
+# MERGE ROUND 1 ----------------------------------------------------------------
+ls $BQSR_DIR/$ROUND"_genotype"/*.vcf >interval_vcf.list
+
+$SINGULARITY split -d -n l/1000 --additional-suffix .list interval_vcf.list m1.
+
+for INTERVAL in $(seq -w 0 999); do
+
+    JOB_NAME="snp.merge_cohort_"$INTERVAL"_"$ROUND
+    THREADS=1
+    LOG="$LOGS_DIR/$JOB_NAME.log" 
+    DEPEND=""
+    SCRIPT="$SUB_SCRIPTS_DIR/$JOB_NAME.sh"
+
+    IN_LIST="m1."$INTERVAL".list"
+    OUT_VCF="m1.$INTERVAL.vcf"
+
+    JOB_QSUB="$QSUB -pe mpi $THREADS -N $JOB_NAME -o $LOG $DEPEND"
+
+    CMD="$SINGULARITY gatk MergeVcfs -I $IN_LIST -O $OUT_VCF -R $REFERENCE"
+
+    DELETE $LOG $SCRIPT
+    SUBMIT "$CMD" "$SCRIPT" "$JOB_QSUB"
+
+done
+
+
+#
+#                               <...wait...>
+#
+#sleep while all jobs are running
+echo "waiting for queue to clear"
+WAIT_FOR_CLEAR_QUEUE
+#
+#                               <...wait...>
+#
+
+
+
+################################################################################
+# Check log files to see that all ran to completion 
+FAILED="1"
+while [ $FAILED -ne 0 ]; do
+    PASSED=0
+    FAILED=0
+    TOTAL=0
+    EXPECTED=1000
+
+    for INTERVAL in $(seq -w 0 999); do
+        JOB_NAME="snp.merge_cohort_"$INTERVAL"_"$ROUND
+        THREADS=1
+        LOG="$LOGS_DIR/$JOB_NAME.log" 
+        DEPEND=""
+        SCRIPT="$SUB_SCRIPTS_DIR/$JOB_NAME.sh"
+        JOB_QSUB="$QSUB -pe mpi $THREADS -N $JOB_NAME -o $LOG $DEPEND"
+
+        if [[ $(grep "picard.vcf.MergeVcfs done" $LOG) ]]; then
+            PASSED=$((PASSED+1))
+        else
+            #resubmit with 12 threads (only reason doing this is to "hog" memory
+            #  from an entire node rather than sharing        
+            FAILED=$((FAILED+1))
+            THREADS=12
+    
+            rm $LOG
+            cat $SCRIPT | $JOB_QSUB
+        fi
+
+        NUM_SAMPLES=$((NUM_SAMPLES+1))
+        TOTAL=$((TOTAL+1))
+    done
+done
+
+#
+#                               <...wait...>
+#
+#sleep while all jobs are running
+echo "waiting for queue to clear"
+WAIT_FOR_CLEAR_QUEUE
+#
+#                               <...wait...>
+#
+
+# MERGE ROUND 2 ----------------------------------------------------------------
+rm *.list
+ls m1.*.vcf >interval_vcf.list
+
+$SINGULARITY split -d -n l/100 --additional-suffix .list interval_vcf.list m2.
+
+for INTERVAL in $(seq -w 0 99); do
+
+    JOB_NAME="snp.merge2_cohort_"$INTERVAL"_"$ROUND
+    THREADS=1
+    LOG="$LOGS_DIR/$JOB_NAME.log" 
+    DEPEND=""
+    SCRIPT="$SUB_SCRIPTS_DIR/$JOB_NAME.sh"
+
+    IN_LIST="$WORK_DIR/m2."$INTERVAL".list"
+    OUT_VCF="$WORK_DIR/m2.$INTERVAL.vcf"
+
+    JOB_QSUB="$QSUB -pe mpi $THREADS -N $JOB_NAME -o $LOG $DEPEND"
+
+    CMD="$SINGULARITY gatk MergeVcfs -I $IN_LIST -O $OUT_VCF -R $REFERENCE"
+
+    DELETE $LOG $SCRIPT
+    SUBMIT "$CMD" "$SCRIPT" "$JOB_QSUB"
+
+done
+
+
+#
+#                               <...wait...>
+#
+#sleep while all jobs are running
+echo "waiting for queue to clear"
+WAIT_FOR_CLEAR_QUEUE
+#
+#                               <...wait...>
+#
+
+
+# MERGE ROUND 3 ----------------------------------------------------------------
+rm *.list
+ls m2.*.list >interval_vcf.list
+
+JOB_NAME="snp.merge3_cohort_"$ROUND
+THREADS=1
+LOG="$LOGS_DIR/$JOB_NAME.log" 
+DEPEND=""
+SCRIPT="$SUB_SCRIPTS_DIR/$JOB_NAME.sh"
+
+IN_LIST="interval_vcf.list"
+OUT_VCF="tmp_cohort.vcf"
+
+JOB_QSUB="$QSUB -pe mpi $THREADS -N $JOB_NAME -o $LOG $DEPEND"
+
+CMD="$SINGULARITY gatk MergeVcfs -I $IN_LIST -O $OUT_VCF -R $REFERENCE"
+
+DELETE $LOG $SCRIPT
+SUBMIT "$CMD" "$SCRIPT" "$JOB_QSUB"
+
+# SORT COHORT VCF --------------------------------------------------------------
+SORT_JOB_NAME="snp.sort_cohort"
+THREADS=12
+LOG="$LOGS_DIR/$JOB_NAME.log"
+DEPEND="-hold_jid snp.merge3_cohort_"$ROUND
+SCRIPT="$SUB_SCRIPTS_DIR/$JOB_NAME.sh"
+
+IN_GVCF="tmp_cohort.vcf"
+OUT_GVCF=$ROUND"_vcfs/cohort_raw_"$ROUND.vcf
+
+CMD="$SINGULARITY gatk --java-options "'"-Xmx8G"'" SortVcf -I $IN_GVCF -O $OUT_GVCF"
+
+SORT_QSUB="$QSUB -pe mpi $THREADS -N $SORT_JOB_NAME -o logs/$SORT_JOB_NAME.log"
+
+DELETE $LOG $SCRIPT
+SUBMIT "$CMD" "$SCRIPT" "$JOB_QSUB"
+
+
