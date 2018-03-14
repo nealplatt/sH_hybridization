19acfa50a61c86bb3628e74c55423c6a9f657e95
nealplatt <neal.platt@gmail.com>
2018-03-14 12:39:58 -0500

    initial commit

---
+++ scripts/bqsr_cleanUp.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# SNP calling in S. haemotobium hybridzone(s).
+# NPlatt
+# Neal.platt@gmail.com
+
+# bqsr_cleanUp.sh - remove unneccessary files from this round of bqsr
+
+# TODO(nplatt): first pass
+
+cd $BQSR_DIR
+
+# CLEANUP ----------------------------------------------------------------------
+#rm -r hc_vcf_r2
+#rm -r individual_vcf_r2
+#rm -r r1_bqsr_bams
+#rm -r db_r2
+#rm -r genotype_interval_vcfs_r2
+#rm -r cohort_vcf_r2
+#rm samples_r2.list
+#rm samples.list
+#rm cohort_filtered_snps.g.vcf.idx
+#rm cohort_preBSQR.g.vcf
+#rm cohort_preBSQR.g.vcf.idx
+#rm cohort_preBSQR_rawIndels.g.vcf
+#rm cohort_preBSQR_rawIndels.g.vcf.idx
+#rm cohort_preBSQR_rawSNPS.g.vcf
+#rm cohort_preBSQR_rawSNPS.g.vcf.idx
+#rm cohort_filtered_indels.g.vcf
+#rm cohort_filtered_indels.g.vcf.idx
+#rm cohort_filtered_snps.g.vcf
+#rm cohort_filtered_snps.g.vcf.idx
+#rm cohort_r1_BQSR.g.vcf
+
+
