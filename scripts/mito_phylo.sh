7326afca94f0708a03780ae0d9c4cb62b7dde780
nealplatt <neal.platt@gmail.com>
2018-03-22 09:09:13 -0500

    re-arranging masking for downstream phylogenetics, fixed sed error

--- scripts/mito_phylo.sh
+++ scripts/mito_phylo.sh
@@ -17,7 +17,19 @@ samtools faidx ../../data/genome/schHae_v1.fa AMPZ01026399.1 >schHae_v1_mt.fa
 $SINGULARITY gatk CreateSequenceDictionary --REFERENCE schHae_v1_mt.fa
 samtools faidx schHae_v1_mt.fa 
 
+#find a way to exclude sites that weren't in the vcf file
+$SINGULARITY bedtools sort -i all_mito_reads.bed >tmp
+$SINGULARITY bedtools merge -i tmp >all_mito_reads_merged.bed 
+$SINGULARITY bedtools complement -i all_mito_reads_merged.bed -g schHae_v1_mt.fa.fai >mito_to_exclude_no_baits.bed
 
+#have to physically make a genome file 
+#AMPZ01026399.1  17526
+$SINGULARITY bedtools complement -i all_mito_reads_merged.bed -g schHae_v1_mt.fa.fai >mito_to_exclude_no_baits.bed
+
+#now mask file and create necessary indecies
+$SINGULARITY bedtools maskfasta -fi schHae_v1_mt.fa -fo schHae_v1_mt.masked.fa -bed mito_to_exclude_no_baits.bed
+$SINGULARITY gatk CreateSequenceDictionary --REFERENCE schHae_v1_mt.masked.fa
+samtools faidx schHae_v1_mt.masked.fa 
 
 #get mitochondrial snps
 $SINGULARITY gatk SelectVariants \
@@ -30,23 +42,25 @@ $SINGULARITY gatk SelectVariants \
 for INDIVIDUAL in $(cat ../sample.list); do
     $SINGULARITY gatk SelectVariants \
         -R $REFERENCE \
-        -V mito_variants.vcf  \
+        -V mito_variants.vcf \
         -sn $INDIVIDUAL \
         -O $INDIVIDUAL.mito.vcf
 
 
     java -jar ~/bin/gatk-3.8.0.jar \
         -T FastaAlternateReferenceMaker \
-        -R schHae_v1_mt.fa  \
+        -R schHae_v1_mt.masked.fa  \
         -o $INDIVIDUAL.mito.fas \
         -V mito_variants.vcf
 
     #change the sequence header name
-    sed -i 's/>.*/>'$INDIVIDUAL'##AMPZ01026399.1/'
+    sed -i 's/>.*/>'$INDIVIDUAL'##AMPZ01026399.1/' $INDIVIDUAL.mito.fas
 
 done
 
-#find a way to exclude sites that weren't in the vcf file
+#create a fasta file with all mito sequences
+cat *mito.fas >sh.mito.fas
+
 
 #get samples form ncbi as outgroups
 
@@ -64,11 +78,6 @@ done
 
 
 
-$SINGULARITY bedtools sort -i all_mito_reads.bed >tmp
-$SINGULARITY bedtools merge -i tmp >all_mito_reads_merged.bed 
-$SINGULARITY bedtools complement -i all_mito_reads_merged.bed -g schHae_v1_mt.fa.fai >mito_to_exclude_no_baits.bed
-#have to physically make a genome file 
-#AMPZ01026399.1  17526
-$SINGULARITY bedtools complement -i all_mito_reads_merged.bed -g test >exclude 
+ 
 
 
