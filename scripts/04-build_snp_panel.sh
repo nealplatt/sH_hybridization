5db375d7dd75cb44a19488ad7bbcc0cad1535594
nealplatt <neal.platt@gmail.com>
2018-07-17 11:08:18 -0500

    updating filters with maf

--- scripts/04-build_snp_panel.sh
+++ scripts/04-build_snp_panel.sh
@@ -1,4 +1,3 @@
-mkdir $RESULTS_DIR/build_snp_panel
 cd $RESULTS_DIR/build_snp_panel
 
 #ERR119622 bovis
@@ -21,16 +20,17 @@ cd $RESULTS_DIR/build_snp_panel
 #ERR539856 intercalatum 
 #ERR539857 mattheei
 
-ln -s $SNP_DIR/cohort_raw_bqsr-1.vcf
-ln -s $SNP_DIR/cohort_raw_bqsr-1.vcf.idx
+ln -s $SNP_DIR/cohort_filtered_bqsr-1.vcf
+ln -s $SNP_DIR/cohort_filtered_bqsr-1.vcf.idx
 
 ${ENVIRONMENTS["SINGULARITY"]} \
     gatk SelectVariants \
-        -V cohort_raw_bqsr-1.vcf \
+        -V cohort_filtered_bqsr-1.vcf \
         -select-type SNP \
         -O cohort_snps.vcf \
         -R $HAE_GENOME
-    #** 611,210 SNPs **#
+    #** 617,668 SNPs **#
+
 
 #annotate snps names
 bcftools annotate \
@@ -38,30 +38,31 @@ bcftools annotate \
     cohort_snps.vcf \
     >cohort_snps_annotated.vcf
 
-#filter loci missing genotype calls in 33% of samples
+    
+#filter loci with 85% of samples genotyped
 vcftools \
-    --max-missing 0.33 \
     --vcf cohort_snps_annotated.vcf \
     --missing-site \
      --stdout \
-    | awk '{print $1"\t"$2}' \
     | sed 1d \
-    >keep_site.list
+    | awk '{if ($6>0.15) print $1"\t"$2}' \
+    >remove_site.list
+    #removing 33,255 sites
 
 vcftools \
     --vcf cohort_snps_annotated.vcf \
-    --positions keep_site.list \
+    --exclude-positions keep_site.list \
     --recode \
     --recode-INFO-all \
     --stdout \
-    >cohort_snps_67perSite.vcf
-#610,745 SNPs
+    >cohort_snps_85perSite.vcf
+    #584,413 SNPs
 #-------------------------
 
 #filter individuals not genotyped at 20% of sites.
 #   80% genotyping rate
 vcftools \
-    --vcf cohort_snps_67perSite.vcf \
+    --vcf cohort_snps_85perSite.vcf \
     --missing-indv \
     --stdout \
     >indiv_missing_table.tsv
@@ -70,40 +71,32 @@ cat indiv_missing_table.tsv \
     | awk '$5 >0.20 {{print $1}}' \
     | sed 1d \
     >remove_indiv.list
+    #ERR084970 S. haem (egypt) added manually; three samples from same seq library
+    #SRR433865 S. haem (egypt) added manually; three samples from same seq library
+    #ERR119622 S. bovis
+    #ERR119623 S. curassoni
+    #ERR539850 S. guineensis
+    #ERR539851 S. bovis
+    #ERR539852 S. guineensis
+    #ERR539853 S. bovis
+    #ERR539854 S. intercalatum
+    #ERR539856 S. intercalatum
+    #Sh.TZ_PEM0075.1
+    #Sm.BR_0447.1
+    #Sm.BR_1278.1
+    #Sm.BR_2039.1
+
 
 vcftools \
-    --vcf cohort_snps_67perSite.vcf \
+    --vcf cohort_snps_85perSite.vcf \
     --remove remove_indiv.list \
     --recode \
     --recode-INFO-all \
     --stdout \
-    >cohort_snps_67perIndiv.vcf
-#610,745 SNPs
-#X Individuals (from X)
-#-------------------------
+    >cohort_snps_80perIndiv.vcf
+    #After filtering, kept 104 out of 118 Individuals
+    #After filtering, kept 584,413 out of a possible 584,413 Sites
 
-
-#find only the snps where one margeboweri/mattehi is genotyped
-vcftools \
-    --max-missing-count 2 \
-    --vcf cohort_snps_67perIndiv.vcf \
-    --missing-site \
-    --indv ERR310940 \
-    --indv ERR539855 \
-    --indv ERR539857 \
-    --stdout \
-    | awk '{print $1"\t"$2}' \
-    | sed 1d \
-    >outgroup_keep_site.list
-
-vcftools \
-    --vcf cohort_snps_67perIndiv.vcf \
-    --positions outgroup_keep_site.list \
-    --recode \
-    --recode-INFO-all \
-    --stdout \
-    >cohort_snps_oneOutGeno.vcf
-    #After filtering, kept 534,853 out of a possible 610,745 Sites
 #-------------------------
 
 #only keep bi-allelic sites
@@ -111,9 +104,9 @@ bcftools view \
     -m2 \
     -M2 \
     -v snps \
-    cohort_snps_oneOutGeno.vcf \
-    >cohort_snps_oneOutGeno_biallelic.vcf
-    #After filtering, kept 504,072 sites
+    cohort_snps_80perIndiv.vcf \
+    >cohort_snps_biallelic.vcf
+    #After filtering, kept 579,316 sites
 
 #now get snps that are single locus in mansoni genome.
 #lift over to smansoni coordinates
@@ -122,74 +115,107 @@ bcftools view \
 vcf2bed \
     --do-not-sort \
     --max-mem=2G \
-    <cohort_snps_oneOutGeno_biallelic.vcf \
+    <cohort_snps_biallelic.vcf \
     | awk '{print $1"\t"$2"\t"$3"\t"$4}' \
-    >cohort_snps_oneOutGeno_biallelic.bed
-    #504,072 remaining snps
+    >cohort_snps_biallelic.bed
+    #549,249 remaining snps
 
 #lift to sman coordinates
 ${ENVIRONMENTS["SINGULARITY"]} \
     /usr/software/progressiveCactus/submodules/hal/bin/halLiftover  \
         $RESULTS_DIR/wga/schMan7_vs_schMan1.hal \
         schHae_v1 \
-        cohort_snps_oneOutGeno_biallelic.bed \
+        cohort_snps_biallelic.bed \
         schMan_v7 \
         cohort_snps_schMan.bed
-        #579,260 remaining snps (increase is due to multilocus snps in mansoni genome)
+        #628,324 remaining snps (increase is due to multilocus snps in mansoni genome)
 
 #now lift the vcf to the sman coords (this will remove snp loci not aligned to sman)
 python $WORK_DIR/scripts/lift_over_vcf.py \
     cohort_snps_schMan.bed \
-    cohort_snps_oneOutGeno.vcf \
+    cohort_snps_biallelic.vcf \
     cohort_snps_schMan.vcf
-    #487,655 remaining snps
+    #531,201 remaining snps
 
 #find loci that map to only a single region
 awk '{print $4}' cohort_snps_schMan.bed \
     | sort \
     | uniq -c \
     | awk '$1>1 {print $2}' \
-    >cohort_snps_schMan_multipos_remove.list
-    #45,392 multiloci snps to remove
+    >cohort_snps_schMan_remove.list
+    #48,776 multiloci snps to remove
 
 
 #now look for multiple snps aligned to a single locus
 cut -f1,2,3 cohort_snps_schMan.bed \
     | sort \
     | uniq -c \
-    | awk '$1>1 {print $2"\t"$3}' >cohort_snps_schMan_multipos_remove.pos
-    #4,287 loci annotated wtih multiple snps
+    | awk '$1>1 {print $2"\t"$3}' \
+    >cohort_snps_schMan_remove.pos
+    #4,823 loci annotated wtih multiple snps
 
 #and put them in a vcf
 vcftools \
     --vcf cohort_snps_schMan.vcf \
-    --exclude-positions cohort_snps_schMan_multipos_remove.pos \
-    --exclude cohort_snps_schMan_multipos_remove.list \
+    --exclude-positions cohort_snps_schMan_remove.pos \
+    --exclude cohort_snps_schMan_remove.list \
     --recode \
     --recode-INFO-all \
     --stdout \
-    >cohort_snps_schMan_singlePos.vcf
-    #After filtering, kept 105 out of 105 Individuals
-    #After filtering, kept 442,247 sites
+    >cohort_snps_schMan_singleLocus.vcf
+    #After filtering, kept 104 out of 104 Individuals
+    #After filtering, kept 482,406 out of a possible 531,201 Sites
 
+
+#final check in vcf for multi-position snps
+#check by snp_name
+grep -v "#" cohort_snps_schMan_singleLocus.vcf \
+    | cut -f3 \
+    | sort \
+    | uniq -c \
+    | awk '{if ($1 > 1) print $2"\t"$3}' \
+    >remove.list
+    #0 redundant loci  
+
+#check by pos
+grep -v "#" cohort_snps_schMan_singleLocus.vcf \
+    | cut -f1,2 \
+    | sort \
+    | uniq -c \
+    | awk '{if ($1 > 1) print $2"\t"$3}' \
+    >remove.pos
+    #101 redundant loci
+
+vcftools \
+    --vcf cohort_snps_schMan_singleLocus.vcf \
+    --exclude-positions remove.pos \
+    --recode \
+    --recode-INFO-all \
+    --stdout \
+    >tmp.vcf
+
+mv tmp.vcf cohort_snps_schMan_singleLocus.vcf
+#After filtering, kept 482,204 out of a possible 482,406 Sites
 
 #fix header and sort
-grep -v "contig=<ID=" cohort_snps_schMan_singlePos.vcf \
-    >cohort_snps_schMan_headerless.vcf
+grep -v "contig=<ID=" cohort_snps_schMan_singleLocus.vcf \
+    >headerless.vcf
 
+#add contigs for sman to header
 ${ENVIRONMENTS["SINGULARITY"]} \
     gatk SelectVariants \
         -R $MAN_GENOME \
-        -V cohort_snps_schMan_headerless.vcf \
-        -O cohort_snps_schMan_header.vcf
+        -V headerless.vcf \
+        -O header.vcf
+        #482,204 loci remaining
 
 #may need to be run on titan
-${ENVIRONMENTS["SINGULARITY"]} \
+${ENVIRONMENTS["TITAN SINGULARITY"]} \
     gatk SortVcf \
         -R $MAN_GENOME \
-        -I cohort_snps_schMan_header.vcf \
+        -I header.vcf \
         -O cohort_snps_schMan_panel.vcf
-        #442,247 snp loci across all (sman homologous) chromosomes
+        ##482,204 snp loci across all (sman homologous) chromosomes
 
 #get only (sman homolgous) autosomal snps
 vcftools \
@@ -205,24 +231,119 @@ vcftools \
     --recode-INFO-all \
     --stdout \
     >cohort_snps_schMan_autosomal_panel.vcf
-    #339,022 snp loci across all (sman homologous) chromosomes
+    #370,770 snp loci across autosomal (sman) chromosomes
 
-#filter autosomal snps in LD
-plink \
-    --vcf cohort_snps_schMan_autosomal_panel.vcf \
-    --allow-extra-chr \
-    --indep-pairwise 25 5 0.20 \
-    --out cohort_snps_schMan_autosomal_panel_LD-25-5-2
-    #Pruning complete.  238702 of 339022 variants removed (100,320 remaining).
-
+#presumed sex chromosome snps
 vcftools \
-    --vcf cohort_snps_schMan_autosomal_panel.vcf \
-    --exclude cohort_snps_schMan_autosomal_panel_LD-25-5-2.prune.out \
+    --vcf cohort_snps_schMan_panel.vcf \
+    --chr SM_V7_ZW \
     --recode \
     --recode-INFO-all \
     --stdout \
-    >cohort_snps_schMan_autosomal_panel_LD-25-5-2.vcf
-    #After filtering, kept 105 out of 105 Individuals
-    #After filtering, kept 100320 out of a possible 339022 Sites
+    >cohort_snps_schMan_ZW_panel.vcf
+    #After filtering, kept 111,343 out of a possible 482,204 Sites
+
+#filter all snps for maf
+vcftools \
+    --vcf cohort_snps_schMan_autosomal_panel.vcf \
+    --maf 0.05 \
+    --recode \
+    --recode-INFO-all \
+    --stdout \
+    >cohort_snps_schMan_autosomal_maf05_panel.vcf
+    #After filtering, kept 69,482 out of a possible 370770 Sites
+
+vcftools \
+    --vcf cohort_snps_schMan_ZW_panel.vcf \
+    --maf 0.05 \
+    --recode \
+    --recode-INFO-all \
+    --stdout \
+    >cohort_snps_schMan_ZW_maf05_panel.vcf
+    #After filtering, kept 16,790 out of a possible 111,343 Sites
+
+#filter autosomal snps in LD
+plink \
+    --vcf cohort_snps_schMan_autosomal_maf05_panel.vcf\
+    --allow-extra-chr \
+    --indep-pairwise 25 5 0.20 \
+    --out cohort_snps_schMan_autosomal_maf0_panel_LD
+#Pruning complete.   63,577 of 69,482 variants removed.
 
 
+vcftools \
+    --vcf cohort_snps_schMan_autosomal_maf05_panel.vcf \
+    --exclude cohort_snps_schMan_autosomal_maf0_panel_LD.prune.out \
+    --recode \
+    --recode-INFO-all \
+    --stdout \
+    >cohort_snps_schMan_autosomal_maf05_LD.vcf
+    #After filtering, kept 104 out of 104 Individuals
+    #After filtering, kept 5,905 out of a possible 69,482 Sites
+
+
+
+
+#once all filtering is complete there are five major files
+#-------------------------------------------------------------------------------
+#cohort_snps_schMan_autosomal_panel.vcf         all autosomal snps
+#cohort_snps_schMan_ZW_panel.vcf                all ZW snps
+
+#cohort_snps_schMan_ZW_maf05_panel.vcf          all ZW snps, maf filtered
+#cohort_snps_schMan_autosomal_maf05_panel.vcf   all autosomal snps, maf filtered
+
+#cohort_snps_schMan_autosomal_maf05_LD.vcf      all autosomal snps, maf filtered, ld filterd
+
+#for ease creating soft links
+ln -s cohort_snps_schMan_autosomal_panel.vcf auto.vcf
+ln -s cohort_snps_schMan_ZW_panel.vcf zw.vcf
+
+ln -s cohort_snps_schMan_autosomal_maf05_panel.vcf auto_maf.vcf
+ln -s cohort_snps_schMan_ZW_maf05_panel.vcf zw_maf.vcf
+
+ln -s cohort_snps_schMan_autosomal_maf05_LD.vcf auto_maf_ld.vcf
+
+
+#haem
+#haem - maf
+#haem - LD
+
+cohort_snps_biallelic.vcf
+cohort_snps_schHae_maf05_panel.vcf
+
+vcftools \
+    --vcf cohort_snps_biallelic.vcf \
+    --maf 0.05 \
+    --recode \
+    --recode-INFO-all \
+    --stdout \
+    >cohort_snps_schHae_maf05_panel.vcf
+#After filtering, kept 104 out of 104 Individuals
+#After filtering, kept 101,327 out of a possible 549,249 Sites
+
+#filter autosomal snps in LD
+plink \
+    --vcf cohort_snps_schHae_maf05_panel.vcf\
+    --allow-extra-chr \
+    --indep-pairwise 25 5 0.20 \
+    --out cohort_snps_schHae_maf05_panel_LD
+    #Pruning complete.   89,609 of 101,327 variants removed.
+
+
+vcftools \
+    --vcf cohort_snps_schHae_maf05_panel.vcf \
+    --exclude cohort_snps_schHae_maf05_panel_LD.prune.out \
+    --recode \
+    --recode-INFO-all \
+    --stdout \
+    >cohort_snps_schHae_maf05_LD.vcf
+    #After filtering, kept 104 out of 104 Individuals
+    #After filtering, kept 11,718 out of a possible 101,327 Sites
+
+#cohort_snps_biallelic.vcf                  all biallelic alleles, haem coords
+#cohort_snps_schHae_maf05_panel.vcf         bi-alleles, maf filtered, haem coords
+#cohort_snps_schHae_maf05_LD.vcf     bi-alleles, maf and LD filtered, haem coords
+
+ln -s cohort_snps_biallelic.vcf haem.vcf
+ln -s cohort_snps_schHae_maf05_panel.vcf haem_maf.vcf
+ln -s cohort_snps_schHae_maf05_LD.vcf haem_maf_ld.vcf
