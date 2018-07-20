8032dfe6859b77f4390995adbfa6ab25ad588dfe
nealplatt <neal.platt@gmail.com>
2018-07-20 16:10:00 -0500

    initial commit

--- scripts/19-tajimasD.sh
+++ scripts/19-tajimasD.sh
@@ -11,3 +11,38 @@ cd diversity
 cp ../niger.list .
 cp ../tz.list .
 
+vcftools \
+    --vcf ../beagle/auto_beagle.vcf \
+    --stdout \
+    --TajimaD 25000 \
+    --keep niger.list \
+    | awk '{if ($3>=10) print $0}' \
+    >ne_w25k_maf00_tajd.txt
+
+vcftools \
+    --vcf ../beagle/auto_beagle.vcf \
+    --stdout \
+    --TajimaD 25000 \
+    --keep tz.list \
+    | awk '{if ($3>=10) print $0}' \
+    >tz_w25k_maf00_tajd.txt
+
+
+
+vcftools \
+    --vcf ../beagle/auto_beagle.vcf \
+    --stdout \
+    --TajimaD 25000 \
+    --maf 0.05 \
+    --keep niger.list \
+    | awk '{if ($3>=10) print $0}' \
+    >ne_w25k_maf05_tajd.txt
+
+vcftools \
+    --vcf ../beagle/auto_beagle.vcf \
+    --stdout \
+    --TajimaD 25000 \
+    --maf 0.05 \
+    --keep tz.list \
+    | awk '{if ($3>=10) print $0}' \
+    >tz_w25k_maf05_tajd.txt
