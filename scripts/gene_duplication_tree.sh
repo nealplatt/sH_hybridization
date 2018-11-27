b31752d2e1e1420640e8ffa0702fec72bb11d87c
nealplatt <neal.platt@gmail.com>
2018-11-27 11:15:55 -0600

    updating comments

--- scripts/gene_duplication_tree.sh
+++ scripts/gene_duplication_tree.sh
@@ -1,11 +1,26 @@
-#gene duplications
+#!/bin/bash
+#
+# SNP calling in S. haemotobium hybridzone(s).
+# NPlatt
+# Neal.platt@gmail.com
+
+# gene_duplication_tree.sh - build a tree of m8 peptidases to identify
+#   gene duplication nodes
+
+# Uses a conda to manage the enviroment
+
+#Set up the environment
+source /master/nplatt/schisto_hybridization/scripts/set_env.sh
+source activate snp_calling
+
+cd $RESULTS_DIR
+
+mkdir m8_gene_family_expansion
+cd m8_gene_family_expansion
 
 #get all platyhelminth m8s plus some outgroup metazoans from pfam:
 # outgroups are human, drosophila, celegans, danio
-
-PF01457_selected_sequences.fa
-
-cd /master/nplatt/schisto_hybridization/results/m8_gene_family_expansion
+# DL'd as ->PF01457_selected_sequences.fa
 
 #convert to single line
 fasta_formatter -w0 -i PF01457_selected_sequences.fa >SL.fas
@@ -38,5 +53,6 @@ sed -i 's/:/#/gi' m8_platy_meta_75perc.fas
     -n m8_platy_meta_75perc
 
 #this tree is imported into megas gene duplication wizard and species names maped
+# tree is manipulated in raxml and final tree is polished in inkscape
 
 
